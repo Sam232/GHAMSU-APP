c1d668004c3fc7265d38c4a8ae61eaa4a2bec5d3
Samuel <jread912@gmail.com>
2018-04-10 00:33:59 -0700

    Updated Code For Authentication

--- helper/auth.js
+++ helper/auth.js
@@ -1,6 +1,23 @@
+const mongoose = require("mongoose");
+
+const {Admin} = require("../models/Admin");
+const {SubAdmin} = require("../models/SubAdmin");
+
 module.exports = {
   ensureAdminAuthentication: (req, res, next) => {
     if(req.isAuthenticated()){
+      Admin.findById(res.locals.mainAdmin._id).then((mainAdmin) => {
+        if(mainAdmin){
+          return next();
+        }
+        req.flash("error_msg", "Not Authorised");
+        res.redirect("/admin/");
+      })
+      .catch((err) => {
+        if(err){
+          console.log("Unable To Fetch Admin\'s Details");
+        }
+      });
       return next();
     }
     req.flash("error_msg", "Not Authorised");
@@ -8,6 +25,18 @@ module.exports = {
   },
   ensureSubAdminAuthentication: (req, res, next) => {
     if(req.isAuthenticated()){
+      SubAdmin.findById(res.locals.subAdmin._id).then((subAdmin) => {
+        if(subAdmin){
+          return next();
+        }
+        req.flash("error_msg", "Not Authorised");
+        res.redirect("/subadmin/");
+      })
+      .catch((err) => {
+        if(err){
+          console.log("Unable To Fetch SubAdmin\'s Details");
+        }
+      });
       return next();
     }
     req.flash("error_msg", "Not Authorised");
